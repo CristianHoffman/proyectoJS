a9e0bb3e5123eca0ebf64dc3185cd0cf67fc2c0c
Cristian Hoffman <cristian.hoffman99@gmail.com>
2022-06-23 18:48:52 -0300

    eventos

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-
+//////////PRODUCTOS//////////
 const contenedorProductos = document.getElementById("contenedor-productos")
 
 const productos = [
@@ -8,6 +8,7 @@ const productos = [
     {id: 3, nombre: "Huevo Kinder", precio: 220, img: 'imagenes/kinder.jpg'},
     {id: 4, nombre: "Alfajor Milka", precio: 120, img: 'imagenes/milka.jpg'},
     {id: 5, nombre: "Chupetin Pops", precio: 30, img: 'imagenes/pops.jpg'},
+    {id: 6, nombre: "Papas Lays", precio: 200, img: 'imagenes/papas.jpg'},
 
 ];
 
@@ -16,8 +17,9 @@ const productos = [
 const mostrarProductos = () => {
     productos.forEach(producto => {
         contenedorProductos.innerHTML += `
+        
             <div class="product-cont">
-                <img class= "img-carrito" src="${producto.img}" />
+                 <img class= "img-carrito" src="${producto.img}" />
                 <h2>${producto.nombre}</h2>
                 <p>$${producto.precio}</p>
                 <button id="${producto.id}" class="agregar">Agregar al carrito</button>
@@ -26,6 +28,50 @@ const mostrarProductos = () => {
     });
 }
 
+
 document.addEventListener('DOMContentLoaded', mostrarProductos);
+
+
+
+//////////MOSTRAR CARRITO //////////
+const carrito = [];
+
+
+const agregarAlCarrito = agr => {
+    if (agr.target.classList.contains('agregar')) {
+        const id = agr.target.id;
+        const producto = productos.find(producto => producto.id == id);
+        carrito.push(producto);
+        mostrarCarrito();
+    }
+}
+
+const mostrarCarrito = () => {
+    const contenedorCarrito = document.getElementById('carrito');
+    contenedorCarrito.innerHTML = '<h2>Carrito:</h2>';
+    carrito.forEach(producto => {
+        contenedorCarrito.innerHTML += `
+            <p>${producto.nombre} = $ ${producto.precio}</p>
+        `;
+    });
+}
+
+
+contenedorProductos.addEventListener('click', agregarAlCarrito);
+
+const form = () => {
+  const contenedorFormulario = document.getElementById("contacto")
+  contenedorFormulario.innerHTML += `
+  <h2> Contactanos </h2>
+  <input type="text">`
+} 
+form()
+
+
+
+
+
+
+
 
 
