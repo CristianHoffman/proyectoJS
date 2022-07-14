5e61701a58e561d67dccda4b2d4c8c7f2cb5dce9
Cristian Hoffman <cristian.hoffman99@gmail.com>
2022-07-14 16:51:14 -0300

    fetch

--- js/main.js
+++ js/main.js
@@ -1,38 +1,53 @@
 const contenedorProductos = document.getElementById("contenedor-productos")
 const contenedorCarrito = document.getElementById("carrito")
+const botonVaciar = document.getElementById('vaciar-carrito')
 const precioTotal = document.getElementById('precioTotal')
-
-
-
 let carrito = []
+
 
 document.addEventListener('DOMContentLoaded', () => {
     localStorage.getItem('carrito') && (carrito = JSON.parse(localStorage.getItem('carrito')))
     actualizarCarrito()
 })
 
+document.addEventListener('DOMContentLoaded', () => {
+    fetchProd()
+  })
 
-listaProductos.forEach((producto) =>{
-    const div = document.createElement('div')
-    div.classList.add('producto')
-    div.innerHTML = `
-    <img src= ${producto.img}>
-    <h3>${producto.nombre} </h3>
-    <p class="precioProducto"> $ ${producto.precio} </p>
-   <button id="${producto.id}" class="agregar boton">Agregar al carrito</button>
-    `
-    contenedorProductos.appendChild(div)
-   
-})
+const fetchProd = async () => {
+    try {
+      const res = await fetch('productos.json')
+      const data = await res.json()
+      listaProductos(data)
+        } catch (error) {
+        console.log(error)
+      
+    }
+  }
 
-
+const listaProductos = data => {
+    data.forEach(producto => {
+        const div = document.createElement('div')
+        div.classList.add('producto')
+        div.innerHTML = `
+        <img src= ${producto.img}>
+        <h3>${producto.nombre} </h3>
+        <p class="precioProducto"> $ ${producto.precio} </p>
+       <button id="${producto.id}" class="agregar boton">Agregar al carrito</button>
+        `
+        contenedorProductos.appendChild(div)
+       
+        
+    })
+}
+  
 const agregarAlCarrito = e => {
     if (e.target.classList.contains('agregar')) {
         const id = e.target.id;
         const producto = listaProductos.find(producto => producto.id == id);
         carrito.push(producto);
         actualizarCarrito();
-       
+        localStorage.setItem('carrito', JSON.stringify(carrito))
     }
 }
 
@@ -44,8 +59,12 @@ const eliminarDelCarrito = (e) =>{
     carrito.splice (indice, 1)
     actualizarCarrito()
     alertEliminar()
-    
 }
+
+botonVaciar.addEventListener('click', () => {
+    carrito.length = 0
+    actualizarCarrito()
+})
 
 const actualizarCarrito = () => {
     const contenedorCarrito = document.getElementById('carrito');
@@ -55,19 +74,13 @@ const actualizarCarrito = () => {
         
             <p>${producto.nombre}</p>
             <p>Precio: ${producto.precio}</p>
-            <button onclick="eliminarDelCarrito(${producto.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
-        `
-        
-        localStorage.setItem('carrito', JSON.stringify(carrito))
-        });
+            <button onclick="eliminarDelCarrito(${producto.id})"  class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `});
+        precioTotal.innerText = carrito.reduce((acc, producto) => acc + producto.cantidad * producto.precio, 0) 
         
 }
 
-
-contenedorProductos.addEventListener('click', agregarAlCarrito);
-
-precioTotal.innerText = carrito.reduce((acc, producto) => acc + producto.cantidad * producto.precio, 0) 
-
+contenedorProductos.addEventListener('click', agregarAlCarrito) ;
 
 
 
--- js/sweetalert.js
+++ js/sweetalert.js
@@ -17,16 +17,4 @@ const alertEliminar = () => {
       })
  }
 
-const agregarToastCarrito = () => {
-  toast.success('🦄 Wow so easy!', {
-    position: "top-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    });
-}
-
 
